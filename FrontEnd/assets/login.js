4f601206f2eb53eb473a0f6bce07e012c0bdda91
Thierry-E <76159642+Thierry-E@users.noreply.github.com>
2023-11-23 16:01:55 +0100

    Modification de l'implantation du lien "modifier de la section "portfolio".

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,76 +1,76 @@
 /****** Variables Globales******/
 
 // Récupération de l'input "Submit"
-const inputSubmit = document.querySelector("#btn-login");
+const inputSubmit = document.querySelector('#btn-login')
 
 /****** Les Fonctions******/
 
 // Gestion de la connexion
 function login() {
-  console.log("lecture fonction login");
+  console.log('lecture fonction login')
   // contacte de l'api.
-  const apiUrl = "http://localhost:5678/api/users/login";
+  const apiUrl = 'http://localhost:5678/api/users/login'
   // les données transmises à l'api.
   //Récupération de l'input "Email"
-  const email = document.querySelector("#email").value;
+  const email = document.querySelector('#email').value
   // Récupération de l'input "Password"
-  const password = document.querySelector("#password").value;
-  console.log("email", email);
+  const password = document.querySelector('#password').value
+  console.log('email', email)
   const data = {
     email: email,
     password: password,
     //Utilisation de ".value", pour obtenir la valeur de l'input.
-  };
+  }
 
   //Configuration de requête HTTP
   const requestOptions = {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
-  };
+  }
 
   //Appel de l'API
   fetch(apiUrl, requestOptions)
     .then((response) => {
       if (!response.ok) {
         throw new Error(
-          "Les informations utilisateur/mot de passe ne sont  pas correctes"
-        );
+          'Les informations utilisateur/mot de passe ne sont  pas correctes'
+        )
       }
       // Demande à avoir un retour de la réponse JSON pour le traitement dans le prochain bloc then.
-      return response.json();
+      return response.json()
     })
 
     .then((authUser) => {
       //Stockage de L'ID et du token de l'utilisateur..
-      console.log("authUser", authUser);
-      const userId = authUser.userId;
-      const authToken = authUser.token;
-      localStorage.setItem("userId", userId);
-      localStorage.setItem("authToken", authToken);
+      console.log('authUser', authUser)
+      const userId = authUser.userId
+      const authToken = authUser.token
+      localStorage.setItem('userId', userId)
+      localStorage.setItem('authToken', authToken)
 
       //Affichage des informations récupérées via la console.
-      console.log("userId", userId);
-      console.log("authToken", authToken);
+      console.log('userId', userId)
+      console.log('authToken', authToken)
 
       // Si les informations transmises sont correcte on redire vers la page d'accueil du site.
-      window.location.href = "../index.html";
+      window.location.href = '../index.html'
     })
 
     .catch((error) => {
       //Affichage d'un message d'erreur
       alert(
-        "Erreur de connexion, merci de vérifier vos identifiants de connexion."
-      );
-    });
+        'Erreur de connexion, merci de vérifier vos identifiants de connexion.'
+      )
+    })
 }
 
 /******Gestionnaire d'événements ******/
 if (inputSubmit) {
-  inputSubmit.addEventListener("click", (event) => {
-    event.preventDefault(); // Empêche le formulaire de se soumettre
-    login(); // Appelle la fonction login lorsque le bouton est cliqué
-  });
+  inputSubmit.addEventListener('click', (event) => {
+    event.preventDefault() // Empêche le formulaire de se soumettre
+    login() // Appelle la fonction login lorsque le bouton est cliqué
+  })
 }
--- FrontEnd/assets/scripts.js
+++ FrontEnd/assets/scripts.js
@@ -10,107 +10,107 @@
 
 /******Variables Globales******/
 //Récupération de l'élément du DOM qui contiendra les projets
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery')
 
 // Récupération de l'élément du DOM qui contiendra les boutons
-const filters = document.querySelector(".filters");
+const filters = document.querySelector('.filters')
 
 /******Les Fonctions******/
 
 // Récupération des projets depuis l'API et affichage de ces derniers sans filtrage.
 async function projectsRecovery() {
-  const url = "http://localhost:5678/api/works";
+  const url = 'http://localhost:5678/api/works'
   fetch(url)
     .then((resp) => resp.json())
     .then((data) => {
-      console.log(data);
+      console.log(data)
 
       data.forEach((projects) => {
         //Création d'une balise dédié à un projet
-        const figureElement = document.createElement("figure");
+        const figureElement = document.createElement('figure')
         // // Création des balises
-        const imageElement = document.createElement("img");
-        imageElement.src = projects.imageUrl;
-        const captionElement = document.createElement("figcaption");
-        captionElement.innerText = projects.title;
+        const imageElement = document.createElement('img')
+        imageElement.src = projects.imageUrl
+        const captionElement = document.createElement('figcaption')
+        captionElement.innerText = projects.title
         //Rattachement de la balise "figure" à la div "gallery".
-        gallery.appendChild(figureElement);
+        gallery.appendChild(figureElement)
         // Rattachement des balises "img" et "figcaption" à l'élément "figure".
-        figureElement.appendChild(imageElement);
-        figureElement.appendChild(captionElement);
-      });
-    });
+        figureElement.appendChild(imageElement)
+        figureElement.appendChild(captionElement)
+      })
+    })
 }
 
 // Création des boutons de filtrage.
 async function categories() {
   // Récupération des catégories depuis l'API
-  const url = "http://localhost:5678/api/categories";
+  const url = 'http://localhost:5678/api/categories'
   fetch(url)
     .then((resp) => resp.json())
     .then((category) => {
       category.forEach((projectsFilters) => {
-        console.log(projectsFilters);
+        console.log(projectsFilters)
 
         // Génération des boutons
-        const button = document.createElement("button");
-        button.classList.add("btnFilters");
-        button.id = projectsFilters.id;
-        button.innerText = projectsFilters.name;
-        filters.appendChild(button);
-      });
-    });
+        const button = document.createElement('button')
+        button.classList.add('btnFilters')
+        button.id = projectsFilters.id
+        button.innerText = projectsFilters.name
+        filters.appendChild(button)
+      })
+    })
 }
 
 async function getWorks() {
-  const requete = await fetch("http://localhost:5678/api/works");
-  return requete.json();
+  const requete = await fetch('http://localhost:5678/api/works')
+  return requete.json()
 }
 
 //Fonction qui crée les projets de façon individuels en fonction de la catégorie.
 function createWork(work) {
   //Récupératiuon de l'élément du DOM qui contiendra les projets
-  const gallery = document.querySelector(".gallery");
+  const gallery = document.querySelector('.gallery')
   //Création d'une balise dédié à un projet
-  const figureElement = document.createElement("figure");
+  const figureElement = document.createElement('figure')
   // const projects = data[i]; //Permet de récupérer les projets un a un
   // Création des balises
-  const imageElement = document.createElement("img");
-  imageElement.src = work.imageUrl;
-  const captionElement = document.createElement("figcaption");
-  captionElement.innerText = work.title;
+  const imageElement = document.createElement('img')
+  imageElement.src = work.imageUrl
+  const captionElement = document.createElement('figcaption')
+  captionElement.innerText = work.title
   // Rattachement des balises "img" et "figcaption" à l'élément "figure".
-  figureElement.appendChild(imageElement);
-  figureElement.appendChild(captionElement);
+  figureElement.appendChild(imageElement)
+  figureElement.appendChild(captionElement)
   //Rattachement de la balise figure à la div "gallery".
-  gallery.appendChild(figureElement);
+  gallery.appendChild(figureElement)
 }
 
 /****** Gestionnaires d'événements******/
 // Mise en place du filtrage des projets via les boutons.
-filters.addEventListener("click", function (event) {
-  const idValue = event.target.id;
+filters.addEventListener('click', function (event) {
+  const idValue = event.target.id
 
   // Vérification si le bouton "Tous" est cliqué.
-  if (idValue === "all") {
+  if (idValue === 'all') {
     // Affichage de tous les projets.
-    gallery.innerHTML = "";
-    projectsRecovery();
+    gallery.innerHTML = ''
+    projectsRecovery()
   } else {
     //affichage des projets en fonction de la catégorie sélectionnée.
-    gallery.innerHTML = "";
+    gallery.innerHTML = ''
     getWorks().then((works) => {
       works.forEach((work) => {
         if (idValue == work.categoryId) {
-          createWork(work);
+          createWork(work)
         }
-      });
-    });
+      })
+    })
   }
-});
+})
 
 //Réinitiamisation de la gallerie via le bouton Tous.
 
 /******Appel des Fonctions******/
-projectsRecovery();
-categories();
+projectsRecovery()
+categories()
